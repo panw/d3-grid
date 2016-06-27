76001d26cdd9089ca1c96342e250347f865e344d
Pan Wangperawong <panthongw@gmail.com>
2016-06-27 15:46:12 -0400

    added highlighting neighbor functionality

--- grid.js
+++ grid.js
@@ -10,6 +10,52 @@ function Grid(w, h, n) {
   this.nodes = this.grid(Array.apply(null, Array(n)).map(function(node, i) { return {id: i} }));
 }
 
+Grid.prototype.toString = function(target, neighbors) {
+  console.log("Node #"+target.id, "{ x:", target.x, "y: ", target.y + "}");
+  console.log("Neighbors:");
+  neighbors.forEach(function(neighbor) {
+    console.log("- Node #"+neighbor, "{ x:", this.nodes[neighbor].x, "y: ", this.nodes[neighbor].y + "}");
+  }.bind(this));
+  console.log("");
+}
+
+Grid.prototype.cordToIndex = function(row, col) {
+  var index = this.numCols * row + col;
+  if(row < this.numRows && col < this.numCols && 
+      row > -1 && col > -1 && index < this.nodes.length)
+    return index;
+  return null;
+}
+
+Grid.prototype.highlightNeighbors = function(node) {
+  var nodeRow = Math.floor(node.id / this.numCols),
+      nodeCol = node.id % this.numCols,
+      neighbors = [],
+      horizontalNeigbhors = [nodeCol - 1, nodeCol + 1],
+      verticalNeighbors = [nodeRow - 1, nodeRow + 1];
+  neighbors = neighbors.concat(horizontalNeigbhors.map(function(neighborCol) {
+    return this.cordToIndex(nodeRow, neighborCol);
+  }.bind(this)));
+
+  neighbors = neighbors.concat(verticalNeighbors.map(function(neighborRow) {
+    return this.cordToIndex(neighborRow, nodeCol);
+  }.bind(this)));
+
+  neighbors = neighbors.filter(Boolean)
+    .sort(function(a, b) { return a - b; });
+  
+  neighbors.forEach(function(neighbor) {
+    if(d3.event.type == "mouseover") {
+      d3.select("#rect-" + node.id).style("opacity", 0.5);
+      d3.select("#rect-" + neighbor).style("opacity", 0.5);
+      this.toString(node, neighbors);
+    } else {
+      d3.select("#rect-" + node.id).style("opacity", 1);
+      d3.select("#rect-" + neighbor).style("opacity", 1);
+    }
+  }.bind(this));
+}
+
 Grid.prototype.render = function() {
   var svg = d3.select("body").append("svg")
     .attr({
@@ -25,7 +71,9 @@ Grid.prototype.render = function() {
     .attr("width", this.grid.nodeSize()[0])
     .attr("height", this.grid.nodeSize()[1])
     .attr("transform", function(d) { return "translate(" + (d.x + 360)+ "," + d.y + ")"; })
-    .style("opacity", 1e-6);
+    .style("opacity", 1e-6)
+    .on("mouseover", this.highlightNeighbors.bind(this))
+    .on("mouseout", this.highlightNeighbors.bind(this));
   rect.transition()
     .attr("width", this.grid.nodeSize()[0])
     .attr("height", this.grid.nodeSize()[1])
